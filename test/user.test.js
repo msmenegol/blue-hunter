86480e9ac0518383fe5986123dc5145293ffeb88
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 16:44:38 -0200

    Add a API test for /user/by-name

--- test/user.test.js
+++ test/user.test.js
@@ -20,5 +20,28 @@ describe('User', function(){
       });
   });
 
+  it('should get all users with \'b\' in the name', function(done){
+    api.get('/user/by-name/b')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) {
+            return done(err);
+        }
+        let users = res.body;
 
+        //i should be getting more than one for this test
+        expect(Array.isArray(users)).to.be.true;
+        expect(
+          for(i=0;i<users.length;i++){
+            //if any name does not contain "b"
+            if(!users[i].fullName.toLowerCase().includes("b")){
+              return false;
+            }
+          }
+          //if all names contained "b"
+          return true;
+        ).to.be.true;
+      });
+  });
 });
