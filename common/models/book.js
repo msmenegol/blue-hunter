e0321f55e94262294238a963673ee62d501da695
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 20:05:57 -0200

    Add byTitle remote method

--- common/models/book.js
+++ common/models/book.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Book) {
+  Book.byTitle = function(title, cb){
+    let response;
+    cb(null,response);
+  }
 
+
+  Book.remoteMethod(
+    'byTitle', {
+      http: {
+        path: '/by-title/:title',
+        verb: 'get'
+      },
+      accepts: {
+        arg: 'title',
+        type: 'string',
+        required: true
+      },
+      returns: {
+        arg: 'books',
+        type: 'array'
+      }
+    }
+  );
 };
