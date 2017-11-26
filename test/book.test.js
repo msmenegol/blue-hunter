b8b19350ecd488008e0cf319f7ad6cd3f87ebafe
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 20:10:42 -0200

    Add first book test

--- test/book.test.js
+++ test/book.test.js
@@ -4,4 +4,21 @@ let expect = require('chai').expect;
 let supertest = require('supertest');
 let api = supertest('http://localhost:3000');
 
-describe('User', function(){});
+describe('User', function(){
+  it('should get all books with \'m\' in the name', function(done){
+    api.get('/book/by-title/m')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) {
+          return done(err);
+        }
+        let books = res.body.books;
+
+        //i should be getting more than one for this test
+        expect(Array.isArray(books)).to.be.true;
+        expect(books.some(book => !book.title.toLowerCase().includes("m"))).to.be.false;
+        return done();
+      });
+  });
+});
