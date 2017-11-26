23fb496c87acfaffd8e3f723a6605e024a7c54e5
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 20:53:24 -0200

    Add second test to author test

--- test/book.byAuthor.test.js
+++ test/book.byAuthor.test.js
@@ -21,4 +21,21 @@ describe('Book - by Author', function(){
         return done();
       });
   });
+
+  it('should get all books with \'Mel\' in the authors name', function(done){
+    api.get('/book/by-author/mel')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) {
+          return done(err);
+        }
+        let books = res.body.books;
+
+        //i should be getting more than one for this test
+        expect(Array.isArray(books)).to.be.true;
+        expect(books.some(book => !book.author.toLowerCase().includes("mel"))).to.be.false;
+        return done();
+      });
+  });
 });
