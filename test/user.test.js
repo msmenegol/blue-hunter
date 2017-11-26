6e569d803d6a093dfdfe8ca4e0eac87d72f6188c
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 20:38:14 -0200

    Remove test from user test

--- test/user.test.js
+++ test/user.test.js
@@ -5,21 +5,6 @@ let supertest = require('supertest');
 let api = supertest('http://localhost:3000');
 
 describe('User', function(){
-  it('should get all users', function(done){
-    api.get('/user')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res) {
-        if(err) {
-          return done(err);
-        }
-        let users = res.body;
-
-        expect(users.length).to.be.above(1);
-        done();
-      });
-  });
-
   it('should get all users with \'b\' in the name', function(done){
     api.get('/user/by-name/b')
       .expect('Content-Type', /json/)
