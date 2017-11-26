a319fb75c03482988a5bcd47ab3459e58edc984e
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 20:52:09 -0200

    Add first test to author test

--- test/book.byAuthor.test.js
+++ test/book.byAuthor.test.js
@@ -5,5 +5,20 @@ let supertest = require('supertest');
 let api = supertest('http://localhost:3000');
 
 describe('Book - by Author', function(){
+  it('should get all books with \'i\' in the authors name', function(done){
+    api.get('/book/by-author/i')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) {
+          return done(err);
+        }
+        let books = res.body.books;
 
+        //i should be getting more than one for this test
+        expect(Array.isArray(books)).to.be.true;
+        expect(books.some(book => !book.author.toLowerCase().includes("i"))).to.be.false;
+        return done();
+      });
+  });
 });
