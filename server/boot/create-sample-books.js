13a2ff88701f7984ac39cd743407a6ed00a52960
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 20:24:13 -0200

    Populate mock books creation script

--- server/boot/create-sample-books.js
+++ server/boot/create-sample-books.js
@@ -12,6 +12,34 @@ if(process.env.NODE_ENV == "dev"){
           price: "$12.50",
           rating: "4/5"
         },
+        {
+          title: 'Moby Dick',
+          author: "@hMelville",
+          yearPublished: 1851,
+          price: "$15.50",
+          rating: "4/5"
+        },
+        {
+          title: 'Guide Me',
+          author: "@mBob",
+          yearPublished: 2010,
+          price: "$40.40",
+          rating: "1/5"
+        },
+        {
+          title: 'Life Guidance Galore',
+          author: "@pRobot",
+          yearPublished: 192038,
+          price: "$42.42",
+          rating: "5/5"
+        },
+        {
+          title: 'I, Robot',
+          author: "@iAsimov",
+          yearPublished: 1950,
+          price: "$20,00",
+          rating: "5/5"
+        }
       ], function(err, mockBooks) {
         if (err) throw err;
 
