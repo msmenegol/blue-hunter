7203860bb27b01f702b844f02a7fb176bcdda7b3
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 20:19:07 -0200

    Create mock books creation script

---
+++ server/boot/create-sample-books.js
@@ -0,0 +1,22 @@
+if(process.env.NODE_ENV == "dev"){
+
+  module.exports = function(app) {
+    app.dataSources.db.automigrate('book', function(err) {
+      if (err) throw err;
+
+      app.models.User.create([
+        {
+          title: '1984',
+          author: "@gOrwell",
+          yearPublished: 1948,
+          price: "$12.50",
+          rating: "4/5"
+        },
+      ], function(err, mockBooks) {
+        if (err) throw err;
+
+        console.log('Models created: \n', mockBooks);
+      });
+    });
+  };
+}
