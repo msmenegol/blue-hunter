397c6bd833934d84af34e945e28c72fc7afd509f
msmenegol <marcelosmenegol@gmail.com>
2017-11-26 17:39:40 -0200

    Add byName registration

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(User) {
-  
+  User.byName = function(cb){
+    let response;
+  }
+  cb(null,response);
+
+  User.remoteMethod(
+    'byName', {
+      http: {
+        path: '/by-name',
+        verb: 'get'
+      },
+      accepts: {
+        arg: 'name',
+        type: 'string',
+        http:{source: 'query'
+      }
+      returns:{
+        arg: 'users',
+        type: 'array'
+      }
+    }
+  );
 };
