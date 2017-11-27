34a546258ba2df1e6a7478175fb41f5a3fc16bb4
msmenegol <marcelosmenegol@gmail.com>
2017-11-27 11:03:12 -0200

    Remove comment

--- server/boot/create-lb-tables.js
+++ server/boot/create-lb-tables.js
@@ -1,13 +1,3 @@
-// var server = require('./server');
-// var ds = server.dataSources.mysql;
-//
-// var lbTables = ['user', 'book'];
-// ds.autoupdate( lbTables, function(er) {
-//   if (er) throw er;
-//   console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
-//   ds.disconnect();
-// });
-
 if(process.env.NODE_ENV != "dev"){
   module.exports = function (app) {
     app.dataSources.mysql.autoupdate();
